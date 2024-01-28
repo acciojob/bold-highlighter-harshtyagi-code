cd3dbba49bff2c1b350f33dff5b1eb8b09177715
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-28 11:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
+	const strongs = document.querySelectorAll("strong");
+	strongs.forEach(strong => {
+		strong.style.color = ("green");
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
+	const strongs = document.querySelectorAll("strong");
+	strongs.forEach(strong => {
+		strong.style.color = ("black");
+	});
+    
+}
 
     
 }
