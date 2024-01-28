d53a84665341804e5cd6ff2515a1f9e19d0f07fe
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-28 11:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function highlight() {
     //Write your code here
-	const strongs = document.querySelectorAll("strong");
-	strongs.forEach(strong => {
-		strong.style.color = ("green");
-	});
+const boldWords = document.querySelectorAll('strong');
+
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.add('bold-highlight');
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-	const strongs = document.querySelectorAll("strong");
-	strongs.forEach(strong => {
-		strong.style.color = ("black");
-	});
-    
-}
+const boldWords = document.querySelectorAll('strong');
 
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.remove('bold-highlight');
+  }
     
 }
